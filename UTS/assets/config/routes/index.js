8ccf11bab08be05a12c1628cd40ba6c15aee1ca3
anggrndewi <dewi.120140149@student.itera.ac.id>
2023-03-22 05:58:31 +0700

    icon navigation

--- UTS/assets/config/routes/index.js
+++ UTS/assets/config/routes/index.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import {Homes, Aktivitas} from "../../page";
@@ -12,10 +12,28 @@ class Index extends Component {
     render() {
         return(
             <NavigationContainer>
-                <materialbtn.Navigator>
-                    <materialbtn.Screen name="Home" component={Homes}/>
-                    <materialbtn.Screen name="Aktivitas" component={Aktivitas}/>
-                    <materialbtn.Screen name="Inbox" component={Inbox}/>
+                <materialbtn.Navigator
+                shifting={false}
+                initialRouteName="Home"
+                activeColor="#09ab54"
+                inactiveColor="#676767"
+                barStyle={{backgroundColor: 'white'}}
+                >
+                    <materialbtn.Screen name="Home" component={Homes} options={{tabBarLabel: 'Home', tabBarIcon :({color}) => (
+                        <View style={{marginTop:-3}}>
+                            <Image source={require('../../images/nav-home.jpg')} style={{height:30, width:30}} />
+                        </View>
+                    ) }}/>
+                    <materialbtn.Screen name="Aktivitas" component={Aktivitas} options={{tabBarLabel: 'Akivitas', tabBarIcon :({color}) => (
+                        <View style={{marginTop:-3}}>
+                            <Image source={require('../../images/nav-activity.jpg')} style={{height:30, width:30}} />
+                        </View>
+                    ) }}/>
+                    <materialbtn.Screen name="Inbox" component={Inbox} options={{tabBarLabel: 'Inbox', tabBarIcon :({color}) => (
+                        <View style={{marginTop:-3}}>
+                            <Image source={require('../../images/nav-inbox.jpg')} style={{height:30, width:30}} />
+                        </View>
+                    ) }}/>
                 </materialbtn.Navigator>
             </NavigationContainer>
         )
