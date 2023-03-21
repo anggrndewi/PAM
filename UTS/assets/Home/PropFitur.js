425baba914cc5ed8424a3c13628868003395f51a
anggrndewi <dewi.120140149@student.itera.ac.id>
2023-03-21 23:28:04 +0700

    Create props

---
+++ UTS/assets/Home/PropFitur.js
@@ -0,0 +1,24 @@
+import { Component } from "react";
+import { Image, Text, View, StyleSheet} from "react-native";
+
+
+class PropFitur extends Component {
+    render(){
+        return (
+            <View style={{width:"25%", alignItems:'center'}}>
+            <Image style={styles.imagefitur} source={this.props.image} />
+            <Text style={{alignSelf:'center'}}>{this.props.title}</Text>
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({ 
+    imagefitur: {
+      height:45,
+      width:45,
+      marginTop:15,
+      marginHorizontal:15,
+    },
+})
+
+export default PropFitur;
