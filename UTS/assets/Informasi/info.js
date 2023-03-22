00344cca027d1616517a9c8d5dfe78e53908de81
anggrndewi <dewi.120140149@student.itera.ac.id>
2023-03-22 15:53:22 +0700

    Update aktivity

--- UTS/assets/Informasi/info.js
+++ UTS/assets/Informasi/info.js
@@ -1,6 +1,6 @@
 export default {
     judul : "Pariwisata.ygy",
-    desk : "Dengan berwisata di YGY, anda dapat merasakan kebahagian yang tidak bisa tergambarkan dan dapat menghilangkan rasa lelah.",
+    desk : "Dengan berwisata di YGY, anda dapat merasakan kebahagian yang tidak bisa tergambarkan dan dapat menghilangkan rasa lelah. Berikut rekomendasi wisata yang menyenangkan !!!",
     penulis : {
         Nama : "Dewi Anggraini",
         NIM  : 120140149,
--- UTS/assets/page/Aktivity/index.js
+++ UTS/assets/page/Aktivity/index.js
@@ -1,13 +1,81 @@
-import React, {Component} from "react";
-import {Text, View} from 'react-native';
+import react, { Component } from "react";
+import { Text, View, Image, StyleSheet, Dimensions, StatusBar, ScrollView } from "react-native";
+import frame from '../../images/awan.jpg';
+import info from "../../Informasi/info";
+import Itempromo from '../../Home/Itempromo';
+import Footer from "../../Footer/footer";
 
-class Aktivitas extends Component {
-    render () {
-        return(
-            <View>
-                <Text>Ini Aktivity</Text>
-            </View>
-        )
-    }
+class Homes extends Component {
+  render(){
+    return(
+      
+      <ScrollView>
+        <StatusBar barStyle='dark-content' translucent backgroundColor="rgba(0, 0, 0, 0)" />
+        <Image style={styles.frame} source={frame} />
+        <Text style={styles.greeting}>{info.judul}</Text>
+
+        <View style={styles.wrapper}>
+          <View style={styles.textwrapper}>
+            <Text style={{justifyContent:'center', fontSize:17}}>{info.desk}</Text>
+          </View>
+        </View>
+
+        <View style={styles.pemisah}></View>
+        <Itempromo />
+        <Footer />
+      </ScrollView>
+    );
+  }
 }
-export default Aktivitas;
+
+const {height, width} = Dimensions.get('window')
+const styles = StyleSheet.create({
+  wrapper: {
+    marginHorizontal:18,
+    height: 120,
+    marginTop: -60,
+    backgroundColor: "white",
+    elevation:4,
+    borderRadius:10,
+  },
+
+  textwrapper: {
+    flexDirection:'row',
+    justifyContent:"space-between",
+    marginHorizontal:12,
+    marginTop:10,
+    fontSize:16,
+    fontWeight:"bold",
+    color:"#383838",
+  },
+
+  line: {
+    height:0.8,
+    backgroundColor:"#adadad",
+    marginTop:10,
+    marginHorizontal:10,
+  },
+
+  frame : { 
+    height:138,
+    width: width,
+  },
+
+  greeting: {
+    fontSize:17,
+    fontWeight:'bold',
+    position:'absolute',
+    alignSelf: "center",
+    top:50,
+    color:"#383838",
+  },
+
+  pemisah:{
+    width: width,
+    height:15,
+    backgroundColor:'#ededed',
+    marginVertical:15,
+  },
+
+})
+export default Homes;
